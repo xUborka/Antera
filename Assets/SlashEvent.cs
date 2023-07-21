a1fcd68a386b5379b9707e57df9e1cff5a19236e
xUborka <xgurke@gmail.com>
2023-07-21 23:49:27 +0200

    Shader testing

---
+++ Assets/SlashEvent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashEvent : MonoBehaviour
+{
+    public ParticleSystem slash_particle;
+
+    public void PrintEvent()
+    {
+        slash_particle.Play();
+    }
+}
